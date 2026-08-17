a531abf948f8fa152000cf0e5100592c0a5df194
Papak Water <papakabcdef@gmail.com>
2026-08-17 15:53:14 +0300

    Refactor array reversal program with improved comments
    Updated comments and formatting in the array reversal program.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,18 +1,20 @@
 ﻿#include <iostream>
 #include<utility>
-// C++ program to reverse an array using pointers
+// C++ poınter kullanarak diziyi tersine çevirme
     int main() {
+		//statik dizi oluşturuldu
 	int dark[6] = {3,2,1,6,5,4};
 	int boyut = 6;
-	int* mi = dark; 
-
+	int* mi = dark;
 	 int* zah = dark + (boyut - 1);
+		//döngü ile ters çevrildi
 	while (&mi < &zah) {
 		std::swap(*mi, *zah);
 
 		mi++;
 		zah--;
 	}
+		//yazdırıldı
 	std::cout << "ters çevrilmiş dizi: \n";
 	for (int i = 0; i < boyut;i++) {
 		std::cout <<dark[i]<<" ";
